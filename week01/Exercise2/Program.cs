b582e194331037ad7ab09a297672c8b1dabdafb1
Laitesha <llamb@byupathway.edu>
2026-01-06 16:03:08 -0700

    WEEK 1 (ex:2)

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade average?");
+        string usersGradeAverage = Console.ReadLine();
+        int x = int.Parse(usersGradeAverage);
+        
+        string letter = "";
+
+        if  ( x >= 90)
+        {
+            letter = "A";
+            Console.WriteLine("Congratulations!");
+        }
+        else if ( x >= 80 )
+        {
+            letter = "B";
+            Console.WriteLine("Congratulations!");
+        }
+
+        else if ( x >= 70)
+        {
+            letter = "C";
+            Console.WriteLine("Congratulations");
+
+
+        }
+        else if ( x >= 60)
+        {
+            letter = "D";
+            Console.WriteLine("Maybe Next Time");
+
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine("Maybe Next Time");
+        }
     }
 }
