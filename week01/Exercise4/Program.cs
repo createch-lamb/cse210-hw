2e56af417323751f3a124e794d41f14c7c0f6847
Laitesha <llamb@byupathway.edu>
2026-01-07 12:59:13 -0700

    ex.4 wk1 Completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
-
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        
+
+        List<int> numbers = new List<int>();
+
+        int userNum = -1;
+
+
+
+        while (userNum != 0)
+        {
+            Console.Write("Enter a random number (Select 0 to quit)");
+
+            string userRes = Console.ReadLine();
+            userNum = int.Parse(userRes);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+
+       
+        int sum = 0;
+        foreach (int number in numbers)
+            {
+                sum += number;
+            }
+        Console.WriteLine($"The total is {sum}");
+
+        int average = ((int) sum) / numbers.Count;
+        Console.WriteLine($"Average is: {average}");
+
+        int max = 0;
+        foreach ( int number in numbers )
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+
+        Console.WriteLine($"The largest number is: {max} ");
+            }
+            {
+                
+            }
+        {
+            
+        }
+        }
     }
 }
