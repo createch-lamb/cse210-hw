bc4b6e604fc416690a4c07f2077a1ac3da4dec6a
Laitesha <llamb@byupathway.edu>
2026-01-06 21:52:28 -0700

    Guessing game (Ex.3 wk1)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+        int guess = -1;
+
+        while ( guess != number )
+        {
+
+         Console.WriteLine("Could you guess the magic number?");
+         guess = int.Parse(Console.ReadLine());
+
+         if ( number > guess)
+         {
+            Console.WriteLine("Higher");
+         }
+         else if (number < guess )
+         {
+            Console.WriteLine("Lower");
+         }
+         else 
+         {
+            Console.WriteLine("You did it! Way to go!");
+         }
+        } 
     }
 }
