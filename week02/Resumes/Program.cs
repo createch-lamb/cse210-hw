242bce27b086284ee8e0c274e79469eb5d632cd1
Laitesha <llamb@byupathway.edu>
2026-01-18 22:11:51 -0700

    Resume and Job

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1. _company = "Microsoft";
+        job1. _jobTitle = "Software Engineer";
+        job1. _startYear = 2015;
+        job1. _endYear = 2026;
+
+        
+        Job job2 = new Job();
+        job2. _company = "Apple";
+        job2. _jobTitle = "Web Developer";
+        job2. _startYear = 2001;
+        job2. _endYear = 2012;
+
+        
+
+        Resume myResume = new Resume();
+        myResume. _name = "Tiffany Willard";
+        myResume. _jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
+
+        
+
     }
 }
