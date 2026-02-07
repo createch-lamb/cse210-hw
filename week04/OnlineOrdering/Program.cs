6946168a074ed29a8441f7639fe3e683bc9c8fda
Laitesha <llamb@byupathway.edu>
2026-02-06 22:22:57 -0700

    Online Ordering Project

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("4343 Clue St", "Chitown", "IL", "USA");
+        Customer customer1 = new Customer("Joseph Farlin", address1);
+        Order order1 = new Order(customer1);
+
+        Product product1 = new Product("Sung T.V.", "TV-007", 1299.99, 1);
+        Product product2 = new Product("Remote Controlled Saucer", "RC-567", 30.00, 2);
+        Product product3 = new Product("The Fetcher", "FET-321", 60.00, 1);
+
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+        order1.AddProduct(product3);
+
+        Console.WriteLine("----Order 1----");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost():F2}");
+        Console.WriteLine("\n");
+
+
+        //International Customer
+
+        Address address2 = new Address("123 Marlon Ave", "Toronto", "Ontario", "Canada" );
+        Customer customer2 = new Customer("Stepen Lorde", address2);
+        Order order2 = new Order(customer2);
+
+        Product product4 = new Product("Mobile Phone", "Mobe-459", 200.00, 1);
+        Product product5 = new Product("Foot Massager", "FMA-281", 150.00, 1);
+
+        order2.AddProduct(product4);
+        order2.AddProduct(product5);
+
+        Console.WriteLine("----Order 2----");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost():F2}");
+        Console.WriteLine("\n");
+
+
+        //Order 3 in USA
+
+        Address address3 = new Address("486 Cooper Dr.", "Pendleton", "OR", "USA");
+        Customer customer3 = new Customer("Margie Love", address3);
+        Order order3 = new Order(customer3);
+
+        Product product6 = new Product("Foot Support", "FSP-01", 20.00, 2);
+        Product product7 = new Product("Wireless Earbuds", "EAR-W01", 50.00, 1);
+        Product product8 = new Product("Smart Glasses", "SGL-003", 300.00, 1);
+
+        order3.AddProduct(product6);
+        order3.AddProduct(product7);
+        order3.AddProduct(product8);
+
+        Console.WriteLine("----Order 3----");
+        Console.WriteLine(order3.GetPackingLabel());
+        Console.WriteLine(order3.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order3.CalculateTotalCost():F2}");
+
+
+
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Simple Study Tips", "Ellie's Corner", 1200);
+        video1.AddComment(new Comment("Kristy", "Wonderful!"));
+        video1.AddComment(new Comment("Jackson", "Terrific Video"));
+        video1.AddComment(new Comment("Scott", "Very Detailed"));
+        videos.Add(video1);
+
+        Video video2 = new Video("The Cool Secrets Hidden In Roblox", "Action Jackson", 1800);
+        video2.AddComment(new Comment("Jackie", "Great video. Look forward to more"));
+        video2.AddComment(new Comment("Eliana", "You are so authentic"));
+        video2.AddComment(new Comment("Mariah", "I enjoyed it very much"));
+        video2.AddComment(new Comment("Ruth", "Fantastic"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Basketball Technique For Girls", "Rye Knows Best", 1600);
+        video3.AddComment(new Comment("Connor", "Rad!"));
+        video3.AddComment(new Comment("Christine", "Thank You!"));
+        video3.AddComment(new Comment("Bernie", "Love"));
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.Length} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()} ");
+            Console.WriteLine("Comments: ");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console. WriteLine($"  -  {comment.CommenterName}:  {comment.Text}");
+            }
+
+            Console.WriteLine();
+
+        }
     }
 }
