afa2e1272d8d016f91356524812dccf24ec8bbf2
Laitesha <llamb@byupathway.edu>
2026-02-09 17:06:34 -0700

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Afia Robinson", "Wonders Of The World");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Veronica Calls", "Mixed Fractions", "10.5", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Zoe Delani", "Essay Writing", "Become A Pro at Writing Essays");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
